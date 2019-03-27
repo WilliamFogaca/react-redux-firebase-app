c6c580debf707a24fee03f0512388f99fa8493ad
WilliamFogaca <williamps0810@gmail.com>
2019-03-27 15:38:05 -0300

    Instalação e configuração do firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,18 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyBWv5VRNXst9jWG3wtt4auXTd6cnOCowIw",
+  authDomain: "react-redux-firebase-app-4afc6.firebaseapp.com",
+  databaseURL: "https://react-redux-firebase-app-4afc6.firebaseio.com",
+  projectId: "react-redux-firebase-app-4afc6",
+  storageBucket: "",
+  messagingSenderId: "839785298837"
+};
+
+firebase.initializeApp(config);
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
