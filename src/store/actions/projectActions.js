ad9f864ae5623ae909994f5de0678413157b10b6
WilliamFogaca <williamps0810@gmail.com>
2019-03-27 16:24:21 -0300

    Implementação para adicionar para adicionar dados ao Firestore

--- src/store/actions/projectActions.js
+++ src/store/actions/projectActions.js
@@ -1,5 +1,19 @@
+
+
 export const createProject = (project) => {
-  return (dispatch, getState) => {
-    dispatch({ type: 'CREATE_PROJECT', project })
+  return (dispatch, getState, { getFirebase, getFirestore }) => {
+    const firestore = getFirestore();
+    firestore.collection('projects').add({
+      ...project,
+      authorFirstName: 'William',
+      authorLastName: 'Fogaça',
+      authorId: 12345,
+      createdAd: new Date()
+    }).then(() => {
+      dispatch({ type: 'CREATE_PROJECT', project })
+    }).catch((err) => {
+      dispatch({ type: 'CREATE_PROJECT_ERROR', err })
+    })
+    
   }
 };
--- src/store/reducers/projectReducer.js
+++ src/store/reducers/projectReducer.js
@@ -9,10 +9,13 @@ const initState = {
 const projectReducer = (state = initState, action) => {
   switch(action.type) {
     case 'CREATE_PROJECT':
-      console.log('Created project!', action.project)
-      return state
+      console.log('Created project!', action.project);
+      return state;
+    case 'CREATE_PROJECT_ERROR':
+      console.log('Create project error', action.err);
+      return state;
     default:
-      return state
+      return state;
   }
 }
 
