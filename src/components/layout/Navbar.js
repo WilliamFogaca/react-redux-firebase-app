dc8df140c4fd44bb83de1cd13b6c20e6e18e4e53
WilliamFogaca <williamps0810@gmail.com>
2019-03-28 00:12:43 -0300

    Implementação de SignOut, implementação de para alterar links do navbar e implementação para aguardar o auth do firebase estar pronto

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -5,22 +5,21 @@ import { connect } from 'react-redux'
 import SignedInLinks from './SignedInLinks'
 import SignedOutLinks from './SignedOutLinks'
 
-const Navbar = () => {
+const Navbar = ({ auth }) => {
+  const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
   return (
     <nav className="nav-wrapper grey darken-3">
       <div className="container">
         <Link to='/' className="brand-logo">MarioPlan</Link>
-        <SignedInLinks />
-        <SignedOutLinks />
+        {links}
       </div>
     </nav>
   )
 }
 
 const mapStateToProps = state => {
-  console.log(state);
   return {
-
+    auth: state.firebase.auth
   }
 }
 
